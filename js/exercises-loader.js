d5eb52e7a5ea879e9fb6a4adca7775503aea4744
AlexFCL <46592757+AlexFCL@users.noreply.github.com>
2026-05-13 17:34:44 +0200

    Delete js directory

--- js/exercises-loader.js
+++
@@ -1,9 +0,0 @@
-export async function loadCategories() {
-  const response = await fetch("data/categories.json");
-  return response.json();
-}
-
-export async function loadExercises(categoryId) {
-  const response = await fetch(`data/exercises/${categoryId}.json`);
-  return response.json();
-}
